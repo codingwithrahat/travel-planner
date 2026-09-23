49494c2b0bbcb6c8954f8724eb7ba8ec296cebe7
codingwithrahat <codingwithrahat@gmail.com>
2026-09-23 18:03:25 +0600

    fix error

--- src/main/java/com/example/travelplanner/controller/TravelPlanController.java
+++ src/main/java/com/example/travelplanner/controller/TravelPlanController.java
@@ -9,6 +9,8 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/travel-planner")
